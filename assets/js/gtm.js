b9362c3bb4e8350d1cc52ca9b9e1c5915852779c
Hafizi Ruslan <pizi@hotmail.my>
2023-04-24 05:33:34 +0800

    remove https dependency

--- assets/js/gtm.js
+++ assets/js/gtm.js
@@ -5,6 +5,6 @@
 		j = d.createElement(s),
 		dl = l != 'dataLayer' ? '&l=' + l : '';
 	j.async = true;
-	j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+	j.src = '//www.googletagmanager.com/gtm.js?id=' + i + dl;
 	f.parentNode.insertBefore(j, f);
 })(window, document, 'script', 'dataLayer', 'GTM-TN989NJ');
