a49991db8316d348f5391670711818d394af99cf
Hafizi Ruslan <pizi@hotmail.my>
2023-07-28 02:42:55 +0800

    debug codes

--- assets/js/stt.js
+++ assets/js/stt.js
@@ -4,7 +4,7 @@ sttButton.onclick = function () { scrollToTop() };
 sttButton.style.display = 'none';
 
 function scrollFunction() {
-  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
     sttButton.style.display = 'flex';
   } else {
     sttButton.style.display = 'none';
